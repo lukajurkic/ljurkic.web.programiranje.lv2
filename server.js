74df7000a0ae2d553e7ff5fce646891434dba48e
Luka Jurkic <lukajurkic1@gmail.com>
2025-04-09 12:41:54 +0200

    task2 and task3

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public')); // "posluzuje" index.html
-// Automatski koristi sve iz mape public
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+const fs = require('fs');
+const path = require('path');
+
 app.get('/', (req, res) => {
     res.send("Ili obican tekst ako nema HTML datoteke.");
+});
+app.get('/slike', (req, res) => {
+    const dataPath = path.join(__dirname, 'images.json');
+    const images = JSON.parse(fs.readFileSync(dataPath));
+    res.render('slike', { images });
 });
 app.listen(3000, () => {
     console.log("Server pokrenut na http://localhost:3000");
 });
-app.use(express.static('public'));
